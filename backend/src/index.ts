fef6ac8e090d6b444faccfd3a5b1629017fd33c5
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-08-14 13:07:12 +0530

    adding prisma urls

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,32 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('Sign Up Route')
 })
 
-export default app
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Sign In Route')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Sign In Route')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('sign in route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param('id')
+  console.log(id)
+  return c.text('get blog route')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('Blog bulk here')
+})
+
+export default app;
