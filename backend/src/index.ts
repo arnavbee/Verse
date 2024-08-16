8dc38d0ce9076037d776c20f2f3c38fa7f20ba55
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-08-16 11:40:43 +0530

    too much debug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { jwt, sign, verify } from 'hono/jwt'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
 
 
 
@@ -13,113 +12,11 @@ const app = new Hono<{
 }>();
 
 app.route("/api/v1/user", userRouter);
-app.route("/api/v1/vlog", blogRouter);
-
-app.use('/api/v1/blog/*', async (c, next) => {
-
-  const header = c.req.header("authorization") || "";
-
-  const token = header.split("")[1]
-
-  const response = await verify(header, c.env.JWT_SECRET);
-  if(response.id){
-    next()
-  }
-
-  else {
-    c.status(403);
-    return c.json({error: "unauthorized"})
-  }
+app.route("/api/v1/blog", blogRouter);
 
 
- 
-})
 
 
-app.post('/api/v1/user/signup', async (c) => {
-  const body = await c.req.json();
-  const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
 
-  try {
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
-        name: body.name
-      }
-    })
-
-    const jwt = await sign({
-      id: user.id,
-    }, "c.env.JWT_SECRET")
-
-    return c.text(jwt)}
-
-    catch(e) {
-      console.log(e);
-      c.status(411);
-      return c.text('Invalid')
-    }
-})
-
-
-app.post('/api/v1/signin', async (c) => {
-  const body = await c.req.json();
-  const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-
-  try {
-    const user = await prisma.user.findFirst({
-      where: {
-        email: body.email,
-        password: body.password
-      }
-    })
-
-    if(!user){
-      c.status(403);
-      return c.json({
-        message: "incorrect credentials"
-      })
-    }
-
-    const jwt = await sign({
-      id: user.id,
-      email: user.email
-    }, "c.env.JWT_SECRET")
-
-    return c.text(jwt)
-
-  } catch(e) {
-    console.log(e);
-    c.status(411);
-    return c.text('Invalid')
-  }
-
-	
-
-})
-
-
-app.post('/api/v1/blog', (c) => {
-  return c.text('Sign In Route')
-})
-
-app.put('/api/v1/blog', (c) => {
-  return c.text('sign in route')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-  const id = c.req.param('id')
-  console.log(id)
-  return c.text('get blog route')
-})
-
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('Blog bulk here')
-})
 
 export default app;
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,104 @@
+import {Hono} from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign, verify} from 'hono/jwt'
+import { Bindings } from "hono/types";
+
+
+export const userRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string,
+        JWT_SECRET: string
+
+    }
+}>();
+
+
+userRouter.use('/api/v1/blog/*', async (c, next) => {
+
+    const header = c.req.header("authorization") || "";
+  
+    const token = header.split("")[1]
+  
+    const response = await verify(header, c.env.JWT_SECRET);
+    if(response.id){
+      next()
+    }
+  
+    else {
+      c.status(403);
+      return c.json({error: "unauthorized"})
+    }
+  
+  
+   
+  })
+  
+  
+  userRouter.post('/signup', async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+          datasourceUrl: c.env?.DATABASE_URL	,
+      }).$extends(withAccelerate());
+  
+    try {
+      const user = await prisma.user.create({
+        data: {
+          email: body.email,
+          password: body.password,
+          name: body.name,
+        }
+      })
+  
+      const jwt = await sign({
+        id: user.id,
+      }, "c.env.JWT_SECRET")
+  
+      return c.text(jwt)}
+  
+      catch(e) {
+        console.log(e);
+        c.status(411);
+        return c.text('Invalid')
+      }
+  })
+  
+  
+  userRouter.post('/api/v1/signin', async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+          datasourceUrl: c.env?.DATABASE_URL	,
+      }).$extends(withAccelerate());
+  
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+            email: body.email,
+            password: body.password
+        }
+      })
+  
+      if(!user){
+        c.status(403);
+        return c.json({
+          message: "incorrect credentials"
+        })
+      }
+  
+      const jwt = await sign({
+        id: user.id,
+        email: user.email
+      }, "c.env.JWT_SECRET")
+  
+      return c.text(jwt)
+  
+    } catch(e) {
+      console.log(e);
+      c.status(411);
+      return c.text('Invalid')
+    }
+  
+      
+  
+  })
+
