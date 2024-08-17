9849e50df978fc9a5e14c9bf1457ae5b9fbc3f08
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-08-17 13:49:11 +0530

    fe

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,40 +1,83 @@
-import { ChangeEvent , FC} from "react";
+import { signupInput } from "@arnavbsingh/verse-common";
+import { ChangeEvent , useState} from "react";
 import {Link} from "react-router-dom";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const Auth = ({type}: {type: "signup" | "signin"}) => {
+    const [postInputs, setPostInputs] = useState<signupInput>({
+        name: "",
+        username: "",
+        password:""
+    });
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <div>
+            <div className="px-10">
             <div className="text-3xl font-extrabold">
            Create an Account
-        </div>
+           </div>
         
         <div className="text-slate-400">
             Already have an account? <Link className="pl-2 underline" to={"/signin"}> Login</Link> 
         </div>
- </div>
-</div>
-      
+        </div>
+    
 
-    </div>
+        <div className="pt-8">
+            <LabelledInput label="Username" placeholder="arnavbsingh..." onChange={(e) => {
+                setPostInputs({
+                    ...postInputs,
+                   
+                    username: e.target.value,
+                   
+
+
+                })
+            }} />
+
+     <LabelledInput label="Name" placeholder="Arnav B Singh..." onChange={(e) => {
+                setPostInputs({
+                    ...postInputs,
+                    name: e.target.value,
+                    username: e.target.value,
+                    password: e.target.value
+
+
+                })
+            }} />
+
+
+<LabelledInput label="Password" placeholder="Password" type={"password"} onChange={(e) => {
+                setPostInputs({
+                    ...postInputs,
+                    password: e.target.value
+
+
+                })
+            }} />
+   </div>
+
+</div>
+</div>
+</div>
 }
 
 interface LabelledInputType {
     label: string;
     placeholder: string;
     onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    type?: string
   }
   
-  const LabelledInput: FC<LabelledInputType> = ({ label, placeholder, onChange }) => {
+  function LabelledInput({ label, placeholder, onChange, type }: LabelledInputType) {
     return (
       <div>
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+        <label className="block mb-2 text-sm text-gray-900 font-semibold pt-4">{label}</label>
         <input
           onChange={onChange}
-          type="text"
+          type={type || "text"}
           id="first_name"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
           placeholder={placeholder}
           required
         />
