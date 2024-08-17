86761c0c8df76ab2a1bc6449261d23161c4cf61e
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-08-17 14:00:18 +0530

    edit

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -56,7 +56,9 @@ export const Auth = ({type}: {type: "signup" | "signin"}) => {
                 })
             }} />
 
-<button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
+<button type="button" className="text-white w-full bg-gray-800 hover:bg-gray-900 focus:outline-none 
+focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800
+ dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type == "signup" ? "Sign Up" : "Sign In"}</button>
 
    </div>
 
