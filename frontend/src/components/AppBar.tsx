c5e23f21485deed2e1022f54bc9c617ad8202da4
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-08-19 23:39:50 +0530

    almost done working finally

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -3,14 +3,20 @@ import { Avatar } from "./BlogCard"
 
 export const Appbar = () => {
     return <div className="border-b flex justify-between px-10 py-4 w-full">
-        <Link to={'/blogs'}>
+     
        
         <div className="text-3xl font-bold flex flex-col justify-center cursor-pointer">
+        <Link to={'/blogs'}>
             Verse
-        </div>
         </Link>
+        </div>
+
         <div className="text-2xl">
-            <Avatar name="Arnav" size={10}/>
+      
+       <Link to={`/publish`}>
+        <button type="button" className=" mr-8 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">New</button>
+        </Link>
+        <Avatar name="Arnav" size={10}/>
         </div>
 
     </div>
