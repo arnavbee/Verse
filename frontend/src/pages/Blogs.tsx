61777422c11ff37822ba2b2b06d768a5b9345611
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-08-19 11:01:24 +0530

    edit

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,13 @@
+import { BlogCard } from "../components/BlogCard"
+
+export const Blogs = () => {
+    return <div>
+        <BlogCard 
+        authorName={"arnav b singh"}
+        title={"How to lose weight in 30 days"}
+        content={"low level design"}
+        publishedDate={"24 Feb 2024"}
+
+        />
+    </div>
+} 
